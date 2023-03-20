e6880cc1ffd0377ae89dea4f23a3e8f325d0d5ba
Ulaş Öztürk <98660610+ulasozturkk@users.noreply.github.com>
2023-03-20 21:49:38 +0300

    add: search feature, fix:file names

--- lib/viewmodels/newslistviewmodel.dart
+++ lib/viewmodels/newslistviewmodel.dart
@@ -2,10 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:newsapp/models/newsArticle.dart';
-
-
 import 'package:newsapp/service/webservice.dart';
 import 'package:newsapp/viewmodels/newsvviewmodel.dart';
+
+
 
 class NewsArticleListViewModel extends ChangeNotifier{
 
@@ -13,6 +13,12 @@ class NewsArticleListViewModel extends ChangeNotifier{
 
   NewsArticleListViewModel() {
     _populateTopHeadlines();
+  }
+
+  void search(String keyword) async{
+    List<newsarticle> newsArticle= await Webservice().fetchHeadlinesByKeyword(keyword);
+    this.articles = newsArticle.map((article) => NewsArticleViewModel(article: article)).toList();
+    notifyListeners();
   }
 
   void _populateTopHeadlines() async  {
